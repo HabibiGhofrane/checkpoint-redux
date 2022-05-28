b780a77d3150f75ca5e253af72129dbfe7543bf1
unknown <habibighofrane0000@gmail.com>
2022-05-28 06:16:15 -1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import AddTask from './Components/AddTask/AddTask';
+import ListTask from './Components/ListTask/ListTask';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='toDoContainer'>
+        <h1>My Todo List</h1>
+        <AddTask />
+        <ListTask />
+      </div>
+      
     </div>
   );
 }
 
-export default App;
+export default App ;
+
+
+
+
+
+<h1>tesxte de la bonne humeur et de la joir et juste maghnifique le jour ou te viendra le meilleur moment est juste maghnifique le plus beau moment et la bonne nuit est ole doies$
+  est le jour est le meilleur de la journe et juste maghnifique et merci, ou lkol what en cours de lamoinset le meilleur de la bonne attitude et de juste avoir la bonne abitude et juste maghnifique et merci kenza et kinda  et le jour de la meme attitude et le joule jour et merci est le kenza il faut de voir et merci et le jour ou je veux et merci est le jour est calui est belui est forme et cours et berchellet are to your et merci est our suite est dela est juste et le celui la bonne
+
+</h1>
